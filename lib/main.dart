bfe3f32a2d299b33f45394773b8793c913a90b43
DESKTOP-7EN38LH\LeQuocDat <leods7ven@gmail.com>
2024-06-12 20:59:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:foodeck_driver_app/main_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  return runApp(
+    const MainApp(),
+  );
+}
